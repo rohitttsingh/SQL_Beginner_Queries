a394e185bb4a8eb77a7a36a01879b18674809d0d
Rohit Singh <73903627+rohitttsingh@users.noreply.github.com>
2023-04-19 14:31:46 +0530

    Create SqlQueries.sql

---
+++ SqlQueries.sql
@@ -0,0 +1,29 @@
+/*SELECT - extracts data from a database
+UPDATE - updates data in a database
+DELETE - deletes data from a database
+INSERT INTO - inserts new data into a database
+CREATE DATABASE - creates a new database
+ALTER DATABASE - modifies a database
+CREATE TABLE - creates a new table
+ALTER TABLE - modifies a table
+DROP TABLE - deletes a table
+CREATE INDEX - creates an index (search key)
+DROP INDEX - deletes an index*/
+
+CREATE DATABASE testDB;
+USE testDB;
+DROP DATABASE DatabaseName;
+SELECT * FROM sys.databases;
+
+CREATE TABLE CUSTOMERS(
+   ID   INT              NOT NULL,
+   NAME VARCHAR (20)     NOT NULL,
+   AGE  INT              NOT NULL,
+   ADDRESS  CHAR (25) ,
+   SALARY   DECIMAL (18, 2),       
+   PRIMARY KEY (ID)
+);
+
+SELECT table_name, table_schema
+FROM information_schema.tables
+WHERE table_type = 'BASE TABLE';
